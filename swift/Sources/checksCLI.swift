be1ce3bab035f2dd730430ebaae486e02e0e9a3c
emlunar <emlunar>
2024-08-16 04:20:46 +0000

    add swift

---
+++ swift/Sources/checksCLI.swift
@@ -0,0 +1,19 @@
+// https://www.swift.org/getting-started/cli-swiftpm/
+
+import Figlet
+import ArgumentParser
+
+@main
+struct FigletTool: ParsableCommand {
+    @Option(help: "Specify the input")
+    public var input: String
+
+    public func run() throws {
+        Figlet.say(self.input)
+    }
+
+    // static func main() {
+    //    Figlet.say("Hello, Swift!")
+    // }
+}
+
