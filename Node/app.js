549b77b16a68cbcfada3d88a2d64a0352c4ee703
emlunar <emlunar>
2024-08-22 04:57:09 +0200

    test node

--- Node/app.js
+++ Node/app.js
@@ -1,3 +1,6 @@
+process.env.TZ="Asia/Bangkok";
+console.log("test", new Date());
+console.log(new Date().toLocaleString('th-TH', { timeZone: 'Asia/Bangkok' }));
+console.log(new Date().toLocaleString('en-US', { timeZone: 'Asia/Bangkok' }));
 
-console.log("test");
 
